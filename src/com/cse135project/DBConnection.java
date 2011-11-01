354eae105536ce1cc1043241980fc1d41045a769
Tom He <tomhe123@hotmail.com>
2011-10-31 20:44:26 -0700

    Set up database connection pools

--- src/com/cse135project/DBConnection.java
+++ src/com/cse135project/DBConnection.java
@@ -1,11 +1,29 @@
 package com.cse135project;
 import java.sql.*;
 
+import javax.naming.*;
+import javax.sql.*;
+
 public class DBConnection {
-	public static Connection dbConnect() throws SQLException, ClassNotFoundException {
-		Class.forName("org.postgresql.Driver"); //load the driver
+	public static Connection dbConnect() throws SQLException, ClassNotFoundException, NamingException {
+		//Class.forName("org.postgresql.Driver"); //load the driver
 	
-		return DriverManager.getConnection("jdbc:postgresql:ApplicationSystem",
-            							   "cse135", "cl2um43z"); //connect to the db
+		//return DriverManager.getConnection("jdbc:postgresql://localhost/ApplicationSystem",
+        //    							   "cse135", "cl2um43z"); //connect to the db
+		
+		Context initCtx = new InitialContext();
+		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/ApplicationSystemPool");
+		return ds.getConnection();
+	}
+	
+	public static Connection dbConnectUsers() throws SQLException, ClassNotFoundException, NamingException {
+		//Class.forName("org.postgresql.Driver"); //load the driver
+	
+		//return DriverManager.getConnection("jdbc:postgresql://localhost/ApplicationSystem",
+        //    							   "cse135", "cl2um43z"); //connect to the db
+		
+		Context initCtx = new InitialContext();
+		DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/UsersPool");
+		return ds.getConnection();
 	}
 }
